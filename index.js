30d976baa449b96f732701eb46eaa637564cc9c9
jonnyhak <jonnyhak@gmail.com>
2020-11-15 19:56:37 -0500

    commit tree e19780581a531a2d671e33873e3e9278cc9fd1cd

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+var num1 = 31
+var num2 = 2
+
+let multiply = num1 * num2 
+
+
+let random = Math.random(5, 30)
+
+let mod = 20 % 16 
+
+let max = Math.max(8 , 3, 13, 20, 4, 0)
